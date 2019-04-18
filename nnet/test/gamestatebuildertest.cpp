a244ae8e594101f2ff0fab9fc25d7eb2ce07860f
Nathan Groover <nathan.groover@gmail.com>
2019-04-18 05:09:53 +0000

    Fix gamestatebuilder unit tests

--- nnet/test/gamestatebuildertest.cpp
+++ nnet/test/gamestatebuildertest.cpp
@@ -38,7 +38,7 @@ void GamestateBuilderTest::checkCharacters()
    cs->province3 = 8.0;
 
    // look at CharacterSlot size and do math manually to figure out offset
-   const int baseOffset=21*2;
+   const int baseOffset=22*2;
    CPPUNIT_ASSERT(rawData[baseOffset] == 5.0 &&
       rawData[baseOffset+1] == 6.0 && rawData[baseOffset+2] == 7.0 &&
       rawData[baseOffset+3] == 8.0);
@@ -50,24 +50,26 @@ void GamestateBuilderTest::checkHoldings()
    memset(rawData, 0, sizeof(double)*builder->getTotalSize());
 
    HoldingSlot *hs = builder->getHolding(0);
-   hs->province1 = 1.0;
-   hs->province2 = 2.0;
-   hs->province3 = 3.0;
-   hs->province4 = 4.0;
+   hs->in_deck = 1.0;
+   hs->province1 = 2.0;
+   hs->province2 = 3.0;
+   hs->province3 = 4.0;
+   hs->province4 = 5.0;
 
-   const int baseOffset=21*3;
+   const int baseOffset=22*3;
    CPPUNIT_ASSERT(rawData[baseOffset] == 1.0 &&
       rawData[baseOffset+1] == 2.0 && rawData[baseOffset+2] == 3.0 &&
-      rawData[baseOffset+3] == 4.0);
+      rawData[baseOffset+3] == 4.0 && rawData[baseOffset+4] == 5.0);
 
    hs = builder->getHolding(3);
-   hs->province1 = 5.0;
-   hs->province2 = 6.0;
-   hs->province3 = 7.0;
-   hs->province4 = 8.0;
+   hs->in_deck = 5.0;
+   hs->province1 = 6.0;
+   hs->province2 = 7.0;
+   hs->province3 = 8.0;
+   hs->province4 = 9.0;
 
-   const int baseOffset2=21*3+(5*3);
+   const int baseOffset2=22*3+(6*3);
    CPPUNIT_ASSERT(rawData[baseOffset2] == 5.0 &&
       rawData[baseOffset2+1] == 6.0 && rawData[baseOffset2+2] == 7.0 &&
-      rawData[baseOffset2+3] == 8.0);
+      rawData[baseOffset2+3] == 8.0 && rawData[baseOffset2+4] == 9.0);
 }
