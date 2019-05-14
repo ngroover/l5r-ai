dd4d23d8b40da820412df152ad2961f7523e0894
Nathan Groover <nathan.groover@gmail.com>
2019-05-14 03:32:56 +0000

    Update doxygen

--- cards/include/carddata.h
+++ cards/include/carddata.h
@@ -5,7 +5,7 @@
 
 namespace l5r
 {
-   /** An enum describing the type of card.
+   /*! \brief An enum describing the type of card.
    */
    enum class cardtype
    {
@@ -17,7 +17,7 @@ namespace l5r
       invalid
    };
 
-   /** An enum describing a type of dynasty card
+   /*! \brief An enum describing a type of dynasty card
    */
    enum class dynastycardtype
    {
@@ -26,7 +26,7 @@ namespace l5r
       holding
    };
 
-   /** @brief An immutable class containing a card's static data.
+   /*! \brief An immutable class containing a card's static data.
    
       Any data that does not change during the course of the game (such as
       base military/political skill or fate cost) should go here.
@@ -44,7 +44,7 @@ namespace l5r
       public:
          class builder;
 
-         /** @brief Constructor for CardData. Use builder class for easier construction.
+         /*! \brief Constructor for CardData. Use builder class for easier construction.
          */
          CardData(std::string name, cardtype type,
               bool canBeStronghold, int provinceStr,
@@ -54,64 +54,64 @@ namespace l5r
               bool miiltaryDash, bool politicalDash,
               int holdingBonus);
 
-         /** @brief Deconstructor
+         /*! \brief Deconstructor
          */
          ~CardData();
 
-         /** @brief Name of card
+         /*! \brief Name of card
          */
          const std::string name;
 
-         /** @brief Type of card
+         /*! \brief Type of card
          */
          const cardtype type;
 
-         /** @brief True if province can be chosen as a stronghold province
+         /*! \brief True if province can be chosen as a stronghold province
          */
          const bool canBeStronghold;
 
-         /** @brief Strength of province
+         /*! \brief Strength of province
          */
          const int provinceStr;
 
-         /** @brief Amount of starting honor stronghold provides
+         /*! \brief Amount of starting honor stronghold provides
          */
          const int strongholdHonor;
 
-         /** @brief Amount of fate either character, events, or attachments cost
+         /*! \brief Amount of fate either character, events, or attachments cost
          */
          const int fateCost;
 
-         /** @brief Type of dynasty card. (Applies only to dynasty)
+         /*! \brief Type of dynasty card. (Applies only to dynasty)
          */
          const dynastycardtype dynastyType;
 
-         /** @brief Base military skill
+         /*! \brief Base military skill
          */
          const int militarySkill;
 
-         /** @brief Base political skill
+         /*! \brief Base political skill
          */
          const int politicalSkill;
 
-         /** @brief Base glory skill
+         /*! \brief Base glory skill
          */
          const int glory;
 
-         /** @brief True if character has a miiltary skill dash
+         /*! \brief True if character has a miiltary skill dash
          */
          const bool militaryDash;
 
-         /** @brief True if character has a political skill dash
+         /*! \brief True if character has a political skill dash
          */
          const bool politicalDash;
 
-         /** @brief Amount of defensive bonus provided by a holding
+         /*! \brief Amount of defensive bonus provided by a holding
          */
          const int holdingBonus;
    };
 
-   /** @brief The builder class should make it easier to construct a CardData object.
+   /*! \brief The builder class should make it easier to construct a CardData object.
       
       To construct a CardData object from a card id see the CardGenerator class.
    */
@@ -119,59 +119,59 @@ namespace l5r
    {
       public:
 
-         /** @brief Set the card name. The text shown on the actual card.
+         /*! \brief Set the card name. The text shown on the actual card.
          */
          builder& setName(std::string name);
 
-         /** @brief Set the card type
+         /*! \brief Set the card type
          */
          builder& setType(cardtype type);
 
-         /** @brief Set a boolean if card can be chosen as a stronghold province
+         /*! \brief Set a boolean if card can be chosen as a stronghold province
          */
          builder& setCanBeStronghold(bool canBeStronghold);
 
-         /** @brief Set the strength of a province 
+         /*! \brief Set the strength of a province 
          */
          builder& setProvinceStr(int provinceStr);
 
-         /** @brief Set starting honor a stronghold provides
+         /*! \brief Set starting honor a stronghold provides
          */
          builder& setStrongholdHonor(int strongholdHonor);
 
-         /** @brief Set the fate cost to play the card
+         /*! \brief Set the fate cost to play the card
          */
          builder& setFateCost(int fateCost);
 
-         /** @brief Set the type of dynasty card. (Only applies if type is dynastycard)
+         /*! \brief Set the type of dynasty card. (Only applies if type is dynastycard)
          */
          builder& setDynastyType(dynastycardtype dynastyType);
 
-         /** @brief Set base military skill of character
+         /*! \brief Set base military skill of character
          */
          builder& setMilitarySkill(int militarySkill);
 
-         /** @brief Set base political skill of character
+         /*! \brief Set base political skill of character
          */
          builder& setPoliticalSkill(int politicalSkill);
 
-         /** @brief Set base glory skill of character
+         /*! \brief Set base glory skill of character
          */
          builder& setGlory(int glory);
 
-         /** @brief Set boolean indicating a military dash on character (military skill ignored)
+         /*! \brief Set boolean indicating a military dash on character (military skill ignored)
          */
          builder& setMilitaryDash(bool militaryDash);
 
-         /** @brief Set boolean indicating a political dash on character (political skill ignored)
+         /*! \brief Set boolean indicating a political dash on character (political skill ignored)
          */
          builder& setPoliticalDash(bool politicalDash);
 
-         /** @brief Set defensive bonus provided by holdings. (Only applies if type is holding)
+         /*! \brief Set defensive bonus provided by holdings. (Only applies if type is holding)
          */
          builder& setHoldingBonus(int holdingBonus);
 
-         /** @brief Returns CardData object
+         /*! \brief Returns CardData object
          */
          CardData build() const;
 
