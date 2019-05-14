d4cfa3ef39ddc3bc6d62c8a0e5c774125ea0d930
Nathan Groover <nathan.groover@gmail.com>
2019-05-14 03:08:09 +0000

    Update docs

--- docs/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['l5r_2dai',['l5r-ai',['../md_README.html',1,'']]]
-];
--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,18 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "l",
-  1: "l"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Pages"
 };
 
