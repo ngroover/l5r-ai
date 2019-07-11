97cab8ad048a043196d4696c2f90b046c2eded59
Nathan Groover <nathan.groover@gmail.com>
2019-07-11 03:02:57 +0000

    Convert card test to gtest

--- cards/test/carddatageneratortest.cpp
+++ cards/test/carddatageneratortest.cpp
@@ -1,74 +1,67 @@
-#include "carddatageneratortest.h"
+#include "carddatagenerator.h"
 #include "carddata.h"
+#include <gtest/gtest.h>
 
 using namespace l5r;
 
-CPPUNIT_TEST_SUITE_REGISTRATION( CardDataGeneratorTest );
 
-void CardDataGeneratorTest::setUp()
-{
-   generator = std::make_unique<CardDataGenerator>("./cards/data/");
-}
+TEST(CardDataGenerator, characterTest) {
+   auto generator = std::make_unique<CardDataGenerator>("./cards/data/");
 
-void CardDataGeneratorTest::tearDown()
-{
-   // nothing to do
-}
-
-void CardDataGeneratorTest::characterTest()
-{
    // check a lion character
    auto lpb = generator->generateCard("lion-s-pride-brawler");
-   CPPUNIT_ASSERT_EQUAL(lpb->fateCost, 3);
-   CPPUNIT_ASSERT_EQUAL(lpb->name, std::string("Lion's Pride Brawler"));
-   CPPUNIT_ASSERT_EQUAL(lpb->id, std::string("lion-s-pride-brawler"));
-   CPPUNIT_ASSERT(lpb->side ==  deckside::dynasty);
-   CPPUNIT_ASSERT_EQUAL(lpb->militarySkill, 3);
-   CPPUNIT_ASSERT_EQUAL(lpb->politicalSkill, 2);
-   CPPUNIT_ASSERT_EQUAL(lpb->glory, 1);
-   CPPUNIT_ASSERT(!lpb->militaryDash);
-   CPPUNIT_ASSERT(!lpb->politicalDash);
-   CPPUNIT_ASSERT(lpb->type == cardtype::character);
-   CPPUNIT_ASSERT(lpb->clan == clantype::lion);
+   ASSERT_EQ(lpb->fateCost, 3);
+   ASSERT_EQ(lpb->name, std::string("Lion's Pride Brawler"));
+   ASSERT_EQ(lpb->id, std::string("lion-s-pride-brawler"));
+   ASSERT_EQ(lpb->side,  deckside::dynasty);
+   ASSERT_EQ(lpb->militarySkill, 3);
+   ASSERT_EQ(lpb->politicalSkill, 2);
+   ASSERT_EQ(lpb->glory, 1);
+
+   ASSERT_TRUE(!lpb->militaryDash);
+   ASSERT_TRUE(!lpb->politicalDash);
+   ASSERT_EQ(lpb->type, cardtype::character);
+   ASSERT_EQ(lpb->clan, clantype::lion);
 
    // check a crane characters
    auto dc = generator->generateCard("doji-challenger");
-   CPPUNIT_ASSERT_EQUAL(dc->fateCost, 3);
-   CPPUNIT_ASSERT_EQUAL(dc->name, std::string("Doji Challenger"));
-   CPPUNIT_ASSERT_EQUAL(dc->id, std::string("doji-challenger"));
-   CPPUNIT_ASSERT(dc->side ==  deckside::dynasty);
-   CPPUNIT_ASSERT_EQUAL(dc->militarySkill, 3);
-   CPPUNIT_ASSERT_EQUAL(dc->politicalSkill, 3);
-   CPPUNIT_ASSERT_EQUAL(dc->glory, 2);
-   CPPUNIT_ASSERT(!dc->militaryDash);
-   CPPUNIT_ASSERT(!dc->politicalDash);
-   CPPUNIT_ASSERT(dc->type ==  cardtype::character);
-   CPPUNIT_ASSERT(dc->clan == clantype::crane);
+   ASSERT_EQ(dc->fateCost, 3);
+   ASSERT_EQ(dc->name, std::string("Doji Challenger"));
+   ASSERT_EQ(dc->id, std::string("doji-challenger"));
+   ASSERT_EQ(dc->side, deckside::dynasty);
+   ASSERT_EQ(dc->militarySkill, 3);
+   ASSERT_EQ(dc->politicalSkill, 3);
+   ASSERT_EQ(dc->glory, 2);
+   ASSERT_TRUE(!dc->militaryDash);
+   ASSERT_TRUE(!dc->politicalDash);
+   ASSERT_EQ(dc->type, cardtype::character);
+   ASSERT_EQ(dc->clan, clantype::crane);
 
    auto steward = generator->generateCard("steward-of-law");
-   CPPUNIT_ASSERT_EQUAL(steward->influenceCost, 1);
+   ASSERT_EQ(steward->influenceCost, 1);
 }
 
-void CardDataGeneratorTest::holdingTest()
-{
+TEST(CardDataGenerator, holdingTest) {
+   auto generator = std::make_unique<CardDataGenerator>("./cards/data/");
+
    // check a holding
    auto storehouse = generator->generateCard("imperial-storehouse");
-   CPPUNIT_ASSERT_EQUAL(storehouse->name, std::string("Imperial Storehouse"));
-   CPPUNIT_ASSERT_EQUAL(storehouse->id, std::string("imperial-storehouse"));
-   CPPUNIT_ASSERT(storehouse->side ==  deckside::dynasty);
-   CPPUNIT_ASSERT(storehouse->type == cardtype::holding);
+   ASSERT_EQ(storehouse->name, std::string("Imperial Storehouse"));
+   ASSERT_EQ(storehouse->id, std::string("imperial-storehouse"));
+   ASSERT_EQ(storehouse->side, deckside::dynasty);
+   ASSERT_EQ(storehouse->type, cardtype::holding);
 }
 
-void CardDataGeneratorTest::strongholdTest()
-{
+TEST(CardDataGenerator, strongholdTest) {
+   auto generator = std::make_unique<CardDataGenerator>("./cards/data/");
    auto yojin = generator->generateCard("yojin-no-shiro");
-   CPPUNIT_ASSERT(yojin->side == deckside::province);
-   CPPUNIT_ASSERT_EQUAL(yojin->influencePool, 10);
+   ASSERT_EQ(yojin->side, deckside::province);
+   ASSERT_EQ(yojin->influencePool, 10);
 }
 
-void CardDataGeneratorTest::provinceTest()
-{
+TEST(CardDataGenerator, provinceTest) {
+   auto generator = std::make_unique<CardDataGenerator>("./cards/data/");
    auto manicured = generator->generateCard("manicured-garden");
-   CPPUNIT_ASSERT(manicured->side == deckside::province);
-   CPPUNIT_ASSERT(manicured->provinceElement == element::air);
+   ASSERT_EQ(manicured->side, deckside::province);
+   ASSERT_EQ(manicured->provinceElement, element::air);
 }
--- cards/test/main.cpp
+++ cards/test/main.cpp
@@ -1,30 +1,8 @@
-#include <cstdlib>
-#include <iostream>
+#include <gtest/gtest.h>
 #include <glog/logging.h>
-
-#include <cppunit/extensions/HelperMacros.h>
-#include <cppunit/ui/text/TestRunner.h>
-#include <cppunit/CompilerOutputter.h>
-
-using namespace std;
-
-int main(int argc, char *argv[])
-{
+ 
+int main(int argc, char **argv) {
    google::InitGoogleLogging(argv[0]);
-
-   // Get the top level suite from the registry
-   CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
-
-   // Adds the test to the list of test to run
-   CppUnit::TextUi::TestRunner runner;
-   runner.addTest( suite );
-
-   // Change the default outputter to a compiler error format outputter
-   runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(),
-                     std::cerr ) );
-   // Run the tests.
-   bool wasSucessful = runner.run();
-
-   // Return error code 1 if the one of test failed.
-   return wasSucessful ? 0 : 1;
+   testing::InitGoogleTest(&argc, argv);
+   return RUN_ALL_TESTS();
 }
